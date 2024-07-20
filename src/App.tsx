953bf1b66cfa8aac22dd9b53271c902896046a3d
Carlos Augusto <carlosmedeiros.dev@gmail.com>
2024-07-20 01:43:01 -0300

    configuração do tailwind e do axios, criação do primeiro componente Nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-function App() {
-  
+import { Nav } from "./components/Nav";
+
+export function App() {
+
 
   return (
     <>
-     
+      <Nav />
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </>
   )
 }
-
-export default App
---
+++ src/components/Nav/index.tsx
@@ -0,0 +1,7 @@
+export function Nav(){
+    return(
+        <nav className="w-full bg-sky-700 h-4">
+
+        </nav>
+    )
+}
